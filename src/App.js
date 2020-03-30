b53f079ca77ce6b72059c0290b611fbc83f1e6f6
jahorinac <registracijae@gmail.com>
2020-03-30 10:43:50 +0200

    002 - function App to class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import CreateProject from './components/CreateProject'
-function App() {
-  return (
-      <BrowserRouter>
-          <div className="App">
-              <h1>Hello !!!!</h1>
-          </div>
-          <Switch>
-              <Route path='/create' component={CreateProject}/>
-          </Switch>
-      </BrowserRouter>
-  );
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <h1>Hello !!!!</h1>
+                </div>
+                <Switch>
+                    <Route path='/create' component={CreateProject}/>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
 }
 
 export default App;
